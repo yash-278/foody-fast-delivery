88965dac1d83ac182b6799e79de234ed052c0668
Yash Kadam <yashkadam278@gmail.com>
2020-01-02 15:38:25 +0530

    first commit

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ function addItemToCart(title, price /* imageSrc*/) {
           <span class="cart-price cart-column">${price}</span>
           <div class="cart-quantity cart-column">
               <input class="cart-quantity-input" type="number" value="1">
-              <button class="btn btn-danger" type="button">REMOVE</button>
+              <button class="btn-sm btn-danger" type="button">REMOVE</button>
           </div>`;
   cartRow.innerHTML = cartRowContents;
   cartItems.append(cartRow);
